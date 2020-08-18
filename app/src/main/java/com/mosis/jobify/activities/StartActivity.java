f309e7be4795d56fb4f3f61640fddc957833f6f3
emailijic <emailijic@elfak.rs>
2020-08-18 02:26:09 +0200

    done start activity

--- app/src/main/java/com/mosis/jobify/activities/StartActivity.java
+++ app/src/main/java/com/mosis/jobify/activities/StartActivity.java
@@ -2,16 +2,31 @@ package com.mosis.jobify.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.mosis.jobify.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class StartActivity extends AppCompatActivity {
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         getSupportActionBar().hide();
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
